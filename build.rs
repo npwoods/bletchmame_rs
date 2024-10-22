395ee4e8603294c206e3c7ad38b2b488661c6fa6
U-ALDEBERAN\Nate <npwoods@mess.org>
2024-10-22 19:39:16 -0400

    BletchMAME finally has an icon
    Special thanks to `nerd4gw` for his artistry and patience!

--- build.rs
+++ build.rs
@@ -1,7 +1,20 @@
-fn main() {
+use std::env;
+
+use winresource::WindowsResource;
+
+fn main() -> std::io::Result<()> {
+	// build Slint stuff
 	slint_build::compile_with_config(
 		"ui/main.slint",
 		slint_build::CompilerConfiguration::new().with_library_paths(vivi_ui::import_paths()),
 	)
 	.unwrap();
+
+	if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+		WindowsResource::new()
+			// This path can be absolute, or relative to your crate root.
+			.set_icon("ui/bletchmame.ico")
+			.compile()?;
+	}
+	Ok(())
 }
