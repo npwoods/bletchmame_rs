82443fc5a9654e10a0c3a2c9e3d47b2f9e7f2141
U-ALDEBERAN\Nate <npwoods@mess.org>
2024-10-26 16:50:39 -0400

    BletchMAME icon is now used on windows at runtime

--- src/guiutils/mod.rs
+++ src/guiutils/mod.rs
@@ -9,6 +9,9 @@ use i_slint_backend_winit::Backend;
 use i_slint_core::items::PointerEvent;
 use i_slint_core::items::PointerEventKind;
 use slint::platform::PointerEventButton;
+use winit::platform::windows::IconExtWindows;
+use winit::platform::windows::WindowAttributesExtWindows;
+use winit::window::Icon;
 use winit::window::WindowAttributes;
 
 type WindowAttributeHookCallback = Box<dyn Fn(WindowAttributes) -> WindowAttributes + 'static>;
@@ -16,7 +19,19 @@ thread_local! {
 	static WINDOW_ATTRIBUTE_HOOK_CALLBACK: RefCell<Option<WindowAttributeHookCallback>> = const { RefCell::new(None) }
 }
 
+fn bletchmame_icon() -> Option<Icon> {
+	#[cfg(target_os = "windows")]
+	let icon = Some(Icon::from_resource(32512, None).unwrap());
+
+	#[cfg(not(target_os = "windows"))]
+	let icon = None;
+
+	icon
+}
+
 fn window_attributes_hook(attrs: WindowAttributes) -> WindowAttributes {
+	let attrs = attrs.with_window_icon(bletchmame_icon());
+	let attrs = attrs.with_taskbar_icon(bletchmame_icon());
 	WINDOW_ATTRIBUTE_HOOK_CALLBACK.with_borrow(|callback| {
 		if let Some(callback) = callback {
 			callback(attrs)
@@ -34,4 +49,12 @@ pub fn init_gui_utils() {
 
 pub fn is_context_menu_event(evt: &PointerEvent) -> bool {
 	evt.button == PointerEventButton::Right && evt.kind == PointerEventKind::Down
+}
+
+#[cfg(test)]
+mod test {
+	#[test]
+	fn bletchmame_icon() {
+		let _ = super::bletchmame_icon();
+	}
 }
