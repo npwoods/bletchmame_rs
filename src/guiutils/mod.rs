ad303f379826d1898993e243d5d725e6fa27f9ad
U-ALDEBERAN\Nate <npwoods@mess.org>
2025-04-15 18:45:16 -0400

    Added support for using the Slint Qt backend
    This is conditionally enabled with a feature (`slint-qt-backend`), and when this feature is enabled there is a `--slint-backend` command line parameter

--- src/guiutils/mod.rs
+++ src/guiutils/mod.rs
@@ -5,10 +5,11 @@ mod hook;
 pub mod menuing;
 pub mod modal;
 
-use i_slint_backend_winit::Backend;
+use anyhow::Result;
 use i_slint_core::items::PointerEvent;
 use i_slint_core::items::PointerEventKind;
 use slint::platform::PointerEventButton;
+use strum::EnumString;
 use winit::window::WindowAttributes;
 
 use crate::guiutils::hook::create_window_attributes_hook;
@@ -17,10 +18,44 @@ fn global_hook(attrs: WindowAttributes) -> WindowAttributes {
 	attrs
 }
 
-pub fn init_gui_utils() {
-	let mut backend = Backend::new().unwrap();
-	backend.window_attributes_hook = create_window_attributes_hook(global_hook);
-	slint::platform::set_platform(Box::new(backend)).unwrap();
+#[derive(Debug, EnumString)]
+pub enum SlintBackend {
+	#[strum(ascii_case_insensitive)]
+	Winit,
+
+	#[cfg(feature = "slint-qt-backend")]
+	#[strum(ascii_case_insensitive)]
+	Qt,
+}
+
+impl Default for SlintBackend {
+	fn default() -> Self {
+		#[cfg(feature = "slint-qt-backend")]
+		let result = Self::Qt;
+
+		#[cfg(not(feature = "slint-qt-backend"))]
+		let result = Self::Winit;
+
+		result
+	}
+}
+
+pub fn init_slint_backend(backend_type: SlintBackend) -> Result<()> {
+	// create the backend
+	let backend = match backend_type {
+		SlintBackend::Winit => {
+			let mut backend = i_slint_backend_winit::Backend::new()?;
+			backend.window_attributes_hook = create_window_attributes_hook(global_hook);
+			Box::new(backend) as Box<_>
+		}
+
+		#[cfg(feature = "slint-qt-backend")]
+		SlintBackend::Qt => Box::new(i_slint_backend_qt::Backend::new()) as Box<_>,
+	};
+
+	// and set it up
+	slint::platform::set_platform(backend)?;
+	Ok(())
 }
 
 pub fn is_context_menu_event(evt: &PointerEvent) -> bool {
