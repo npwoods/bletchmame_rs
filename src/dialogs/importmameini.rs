ef775b8de70f233b57c346cddb70e3d075e5555f
U-ALDEBERAN\Nate <npwoods@mess.org>
2025-08-10 16:32:51 -0400

    The "Import MAME INI" dialog is now functional
    Open issues:
    - The dialog needs a great deal of UI polishing
    - Variables are not factored in when checking to see if items are already present

--- src/dialogs/importmameini.rs
+++ src/dialogs/importmameini.rs
@@ -1,15 +1,22 @@
 use std::any::Any;
 use std::path::Path;
+use std::rc::Rc;
 
 use anyhow::Result;
+use more_asserts::assert_lt;
+use slint::CloseRequestResponse;
+use slint::ComponentHandle;
+use slint::Model;
+use slint::ModelNotify;
+use slint::ModelRc;
+use slint::ModelTracker;
+use slint::ToSharedString;
 use slint::VecModel;
-use slint::{CloseRequestResponse, Model, ModelNotify, ModelRc, ModelTracker, ToSharedString};
 use tokio::sync::mpsc;
 
 use crate::dialogs::SenderExt;
 use crate::dialogs::file::load_file_dialog;
 use crate::guiutils::modal::ModalStack;
-use crate::importmameini::Disposition;
 use crate::importmameini::ImportMameIni;
 use crate::prefs::PrefsPaths;
 use crate::ui::ImportMameIniDialog;
@@ -21,7 +28,7 @@ const MAME_INI_FILE_TYPES: &[(Option<&str>, &str)] = &[(None, MAME_INI_EXTENSION
 pub async fn dialog_import_mame_ini(
 	modal_stack: ModalStack,
 	prefs_paths: impl AsRef<PrefsPaths>,
-) -> Result<Option<ImportMameIni>> {
+) -> Result<Option<Rc<ImportMameIni>>> {
 	// if we can find a MAME.ini, lets point to it
 	let prefs_paths = prefs_paths.as_ref();
 	let (initial_dir, initial_file) = prefs_paths
@@ -38,16 +45,22 @@ pub async fn dialog_import_mame_ini(
 		return Ok(None);
 	};
 
-	// read the options and create the model
-	let model = ImportMameIniModel::new(&ini_path, prefs_paths)?;
-	let model = ModelRc::new(model);
-
 	// prepare the modal dialog
 	let modal = modal_stack.modal(|| ImportMameIniDialog::new().unwrap());
 	let (tx, mut rx) = mpsc::channel(1);
 
+	// read the options and create the model
+	let dialog_weak = modal.dialog().as_weak();
+	let update_cb = move |import_mame_ini: &ImportMameIni| {
+		let can_apply = import_mame_ini.can_apply();
+		dialog_weak.unwrap().set_ok_enabled(can_apply);
+	};
+	let model = ImportMameIniModel::new(&ini_path, prefs_paths, update_cb)?;
+	let model = ModelRc::new(model);
+
 	// add the model
-	modal.dialog().set_entries(model);
+	let model_clone = model.clone();
+	modal.dialog().set_entries(model_clone);
 
 	// set up the "ok" button
 	let tx_clone = tx.clone();
@@ -72,7 +85,11 @@ pub async fn dialog_import_mame_ini(
 	let accepted = modal.run(async { rx.recv().await.unwrap() }).await;
 
 	// if the user hit "ok", return
-	Ok(accepted.then(|| todo!()))
+	let result = accepted.then(move || {
+		let model = ImportMameIniModel::get_model(&model);
+		model.import_ini.clone()
+	});
+	Ok(result)
 }
 
 fn get_initial_paths(mame_executable_path: &str) -> (Option<&'_ Path>, Option<&'_ str>) {
@@ -89,18 +106,36 @@ fn get_initial_paths(mame_executable_path: &str) -> (Option<&'_ Path>, Option<&'
 }
 
 struct ImportMameIniModel {
-	import_ini: ImportMameIni,
+	import_ini: Rc<ImportMameIni>,
+	update_cb: Box<dyn Fn(&ImportMameIni)>,
 	notify: ModelNotify,
 }
 
 impl ImportMameIniModel {
-	pub fn new(path: impl AsRef<Path>, prefs_paths: &PrefsPaths) -> Result<Self> {
+	pub fn new(
+		path: impl AsRef<Path>,
+		prefs_paths: &PrefsPaths,
+		update_cb: impl Fn(&ImportMameIni) + 'static,
+	) -> Result<Self> {
 		let import_ini = ImportMameIni::read_mame_ini(path, prefs_paths)?;
+		let import_ini = Rc::new(import_ini);
+		let update_cb = Box::new(update_cb) as Box<_>;
+
 		let result = Self {
 			import_ini,
+			update_cb,
 			notify: Default::default(),
 		};
+		result.invoke_update_cb();
 		Ok(result)
+	}
+
+	fn invoke_update_cb(&self) {
+		(self.update_cb)(&self.import_ini);
+	}
+
+	pub fn get_model(model: &ModelRc<ImportMameIniDialogEntry>) -> &Self {
+		model.as_any().downcast_ref::<Self>().unwrap()
 	}
 }
 
@@ -112,33 +147,42 @@ impl Model for ImportMameIniModel {
 	}
 
 	fn row_data(&self, row: usize) -> Option<Self::Data> {
-		let (option, disposition) = self.import_ini.entries().get(row)?;
+		let entry = self.import_ini.entries().get(row)?;
 
-		let path_type = option.path_type.to_shared_string();
-		let path = option.value.to_shared_string();
-		let current_disposition = disposition.get().to_shared_string();
+		let path_type = entry.opt.path_type.to_shared_string();
+		let path = entry.opt.value.to_shared_string();
 
-		let dispositions = if disposition.get() == Disposition::AlreadyPresent {
-			vec![Disposition::AlreadyPresent]
-		} else if option.path_type.is_multi() {
-			vec![Disposition::Ignore, Disposition::Supplement]
-		} else {
-			vec![Disposition::Ignore, Disposition::Replace]
-		};
-		let dispositions = dispositions
-			.into_iter()
+		let dispositions = entry
+			.dispositions
+			.iter()
 			.map(|d| d.to_shared_string())
 			.collect::<Vec<_>>();
 		let dispositions = VecModel::from(dispositions);
 		let dispositions = ModelRc::new(dispositions);
 
+		let current_disposition_index = entry.current_disposition_index.get().try_into().unwrap();
+
 		let result = Self::Data {
 			path_type,
 			path,
-			current_disposition,
+			current_disposition_index,
 			dispositions,
 		};
 		Some(result)
+	}
+
+	fn set_row_data(&self, row: usize, data: Self::Data) {
+		// find the entry
+		let entry = self.import_ini.entries().get(row).unwrap();
+
+		// set the new disposition index
+		let current_disposition_index = data.current_disposition_index.try_into().unwrap();
+		assert_lt!(current_disposition_index, entry.dispositions.len());
+		entry.current_disposition_index.set(current_disposition_index);
+
+		// notify
+		self.notify.row_changed(row);
+		self.invoke_update_cb();
 	}
 
 	fn model_tracker(&self) -> &dyn ModelTracker {
