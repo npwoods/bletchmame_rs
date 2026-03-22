b3c93017f82423bafeb3930fd432a08b3de94369
U-ALDEBERAN\Nate <npwoods@mess.org>
2026-03-22 17:10:32 -0400

    Running MAME diagnostics test for each MAME version in parallel
    We used to do this with old BletchMAME, we're going to try to do this again

--- scripts/test_mame_interactions.sh
+++ scripts/test_mame_interactions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -euo pipefail
+set -x
 
 ################################################################################################
 # test_mame_interactions.sh - Use diagnostic features to exercise a particular version of MAME #
@@ -17,19 +18,12 @@ else
     MAME_EXE=mame.exe
 fi
 
-# Abort if anything fails
-set -e
-
 # Download MAME binaries for Windows
 mkdir -p deps
 curl -f -L "https://github.com/mamedev/mame/releases/download/${MAME_VERSION}/${MAME_VERSION}b_64bit.exe" > deps/${MAME_VERSION}b_64bit.exe
 
 # Extract the archive
 7z -y x deps/${MAME_VERSION}b_64bit.exe -odeps/${MAME_VERSION}
-
-# Get `alienar` ROM
-mkdir -p deps/roms
-curl -L "https://www.mamedev.org/roms/alienar/alienar.zip" > deps/roms/alienar.zip
 
 # Ensure we can digest -listxml from this MAME
 ./deps/${MAME_VERSION}/${MAME_EXE} -listxml | ./target/release/bletchmame.exe --process-listxml
