a2748296429eb4ecdecd81b6ede99e4b66481038
U-ALDEBERAN\Nate <npwoods@mess.org>
2026-06-30 20:40:38 -0400

    Adding debug tracing for measuring auditing times

--- src/appstate.rs
+++ src/appstate.rs
@@ -9,6 +9,7 @@ use std::sync::Mutex;
 use std::sync::mpsc::Sender;
 use std::thread::sleep;
 use std::time::Duration;
+use std::time::Instant;
 
 use anyhow::Error;
 use anyhow::Result;
@@ -19,6 +20,7 @@ use smol_str::ToSmolStr;
 use smol_str::format_smolstr;
 use strum::EnumProperty;
 use throttle::Throttle;
+use tracing::debug;
 use tracing::info;
 
 use crate::action::Action;
@@ -1003,6 +1005,8 @@ fn spawn_audit(
 	// create the job
 	let callback_bubble = ThreadLocalBubble::new(callback);
 	let job = Job::new(move |canceller| {
+		let start_instant = Instant::now();
+
 		// we need to invoke actions on the main thread
 		let invoke_action = make_invoke_action(callback_bubble, canceller.clone());
 
@@ -1046,6 +1050,7 @@ fn spawn_audit(
 
 		// signal completion and return
 		invoke_action(Action::AuditComplete, false);
+		debug!(duration=?start_instant.elapsed(), "spawn_audit() job");
 		job_result
 	});
 	Ok(job)
