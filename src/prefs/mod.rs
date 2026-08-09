06b73c3aa0738a59662e037566ee32c516e787b7
U-ALDEBERAN\Nate <npwoods@mess.org>
2026-08-09 15:12:58 -0400

    MAME debugger support now has to be explicitly enabled by the user
    We were previously assuming that `-debug` had minimal overhead, which was not correct.  So now we force the user to explicitly enable debugging.

--- src/prefs/mod.rs
+++ src/prefs/mod.rs
@@ -93,6 +93,9 @@ pub struct Preferences {
 	#[serde(skip_serializing_if = "default_ext::DefaultExt::is_default")]
 	pub history_position: usize,
 
+	#[serde(skip_serializing_if = "default_ext::DefaultExt::is_default")]
+	pub debugging_enabled: bool,
+
 	pub show_stop_warning: bool,
 }
 
@@ -113,6 +116,7 @@ impl Default for Preferences {
 			collections: [].into(),
 			history: [].into(),
 			history_position: 0,
+			debugging_enabled: false,
 			show_stop_warning: true,
 		}
 	}
