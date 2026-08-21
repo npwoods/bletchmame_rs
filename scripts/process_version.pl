6e837eacebfe2ae606c5dfc714bdda62d4147bf4
U-ALDEBERAN\Nate <npwoods@mess.org>
2026-08-21 16:40:10 -0400

    Changes to release artifact preparation - Using `cargo set-version` to set the build version - Changing to use three digit release versions (Rust standard) - Uploaded artifacts are `BletchMAME_[version].[msi|zip]`

--- scripts/process_version.pl
+++ scripts/process_version.pl
@@ -4,33 +4,20 @@
 # process_version.pl - Generates BletchMAME version information in various forms  #
 ###################################################################################
 
-if (!(<STDIN> =~ /v([0-9]+)\.([0-9]+)(\-[0-9]+)?/))
-{
-	die "Cannot process build string";
+# read a line from stdin and extract version
+my $input = <STDIN>;
+if (!defined $input || $input !~ /v([0-9]+)\.([0-9]+)(\-[0-9]+)?/) {
+    die "Cannot process build string";
 }
 my $major = $1;
 my $minor = $2;
-my $subbuild = 0;
-my $build = $3;
+my $build = $3 // '';
 $build =~ s/^\-//;
-if ($build eq "")
-{
-	$build = 0;
-}
+my $delim = '.';
 
-if ($ARGV[0] eq "--versionhdr")
-{
-	# header file
-	print "#define BLETCHMAME_VERSION_COMMASEP	$major,$minor,$subbuild,$build\n";
-	print "#define BLETCHMAME_VERSION_STRING	\"$major.$minor";
-	if ($subbuild != 0 || $build != 0)
-	{
-		print ".$subbuild.$build";
-	}
-	print "\\0\"\n";
+if ($build eq "") {
+	print join($delim, ($major, $minor)) . "\n";
 }
-else
-{
-	# simple version text
-	print "$major.$minor.$subbuild.$build\n";
+else {
+	print join($delim, ($major, $minor, $build)) . "\n";
 }
